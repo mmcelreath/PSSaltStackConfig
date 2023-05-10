086b9a6315184882a5eed483a44838fa085b0b54
Matt McElreath <matt.mcelreath@gmail.com>
2023-05-10 16:44:29 -0400

    Commented out Dot Sourcing of Classes directory

--- PSSaltStackConfig/PSSaltStackConfig.psm1
+++ PSSaltStackConfig/PSSaltStackConfig.psm1
@@ -1,5 +1,5 @@
 #Dot source classes.
-Get-ChildItem -Path $PSScriptRoot\Classes\*.ps1 | Foreach-Object{ . $_.FullName }
+# Get-ChildItem -Path $PSScriptRoot\Classes\*.ps1 | Foreach-Object{ . $_.FullName }
 #Dot source private functions.
 Get-ChildItem -Path $PSScriptRoot\Functions\Private\*.ps1 | Foreach-Object{ . $_.FullName }
 #Dot source public functions.
